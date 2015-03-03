1b7b22015f1e0a30eaa6cfd54019f9beac74794a
tophat8855 <hrfarley@gmail.com>
2015-03-02 21:36:52 -0800

    uses pictures now. Fancy

--- drag.js
+++ drag.js
@@ -1,18 +1,15 @@
 window.addEventListener('load', init);
 
 function init() {
-  var firstBox = document.getElementById("one");
-  var secondBox = document.getElementById("two");
-  var thirdBox = document.getElementById('three');
-  var fourthBox = document.getElementById('four');
-  var fifthBox = document.getElementById('five');
-  var sixthBox = document.getElementById('six');
-  firstBox.addEventListener('dragstart', startDrag);
-  secondBox.addEventListener('dragover', dragTo);
-  secondBox.addEventListener('drop', doDrop);
+  var allTheBoxes = document.getElementsByTagName('img');
+
+  for (var i = 0; i < allTheBoxes.length; i++) {
+    allTheBoxes[i].addEventListener('dragstart', startDrag);
+    allTheBoxes[i].addEventListener('dragover', dragTo);
+    allTheBoxes[i].addEventListener('drop', doDrop);
+  }
 
   function startDrag(e) {
-    e.dataTransfer.setData("text", e.target.innerHTML);
     e.dataTransfer.setData("id", e.target.id);
   }
 
@@ -22,13 +19,11 @@ function init() {
 
   function doDrop(e) {
     var draggedItemId = e.dataTransfer.getData('id');
-    console.log(draggedItemId);
     var draggedItem = document.getElementById(draggedItemId);
     var newPosition = this;
-    var holder = draggedItem.innerHTML;
-    draggedItem.innerHTML = newPosition.innerHTML;
-    newPosition.innerHTML = holder;
+    var holder = draggedItem.src;
+    draggedItem.src = newPosition.src;
+    newPosition.src = holder;
     e.preventDefault();
-
   }
 }
